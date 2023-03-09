ca853f27b14271609943455f827ad172bc3e12ac
Nikolay <nikolayrudakov@mail.ru>
2023-03-10 00:15:06 +0600

    Initial commit.

---
+++ script.js
@@ -0,0 +1,7 @@
+const p_tag_1stItem = document.querySelector(".error");
+const p_tag_2ndItem = document.querySelector("body > div.error");
+const p_tag_lists = document.querySelectorAll(".error");
+
+console.log(p_tag_1stItem);
+console.log(p_tag_2ndItem);
+console.log(p_tag_lists);

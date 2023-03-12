8cfb71d26886e8d84d98c4286b6fbeb7b52ae4cf
Nikolay <nikolayrudakov@mail.ru>
2023-03-12 21:01:54 +0600

    EoD commit: switch to laptop

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
-const p_tag_1stItem = document.querySelector(".error");
-const p_tag_2ndItem = document.querySelector("body > div.error");
-const p_tag_lists = document.querySelectorAll(".error");
+// const p_tag_1stItem = document.querySelector(".error");
+// const p_tag_2ndItem = document.querySelector("body > div.error");
+// const p_tag_lists = document.querySelectorAll(".error");
 
-console.log(p_tag_1stItem);
-console.log(p_tag_2ndItem);
-console.log(p_tag_lists);
+// console.log(p_tag_1stItem);
+// console.log(p_tag_2ndItem);
+// console.log(p_tag_lists);
+
+// let add_text = document.querySelector('p');
+// add_text.innerText += " Wild thing.";
+
+// let add_texts = document.querySelectorAll('p');
+
+// add_texts.forEach(replace_text => {
+//     replace_text.innerText += ' This text was added by ".forEach"'
+// })
+
+// let content_change = document.querySelector('.content')
+
+// let person_list = ['Nick', 'Tom', 'Bill'];
+
+// person_list.forEach(person => {
+//     content_change.innerHTML += `<p>${person}</p>`;
+// })
+
+// const link = document.querySelector('a');
+// console.log(link);
+// link_attribute = link.getAttribute('href');
+// console.log(link_attribute);
+// link.setAttribute('href', 'https://www.yandex.ru');
+// link.innerText = "Yandex search";
+
+// const new_class = document.querySelector("p")
+// new_class.setAttribute('class', 'success')
+
+// const title = document.querySelector('h1');
+// console.log(title);
+
+// title.style.fontSize = '60px';
+// title.style.marginTop = '10px';
+// title.style.color = 'rgb(235, 169, 0)';
+
+const change_class = document.querySelector(`div > p.error`)
+console.log(change_class);
